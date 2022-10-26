23b7a058565e1092e5eefdf8d25b00e9f2c752bc
SamoK99 <56873602+SamoK99@users.noreply.github.com>
2022-10-26 09:47:32 +0200

    login-register

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:gymnotes/views/login_view.dart';
+import 'package:gymnotes/views/register_view.dart';
 
 import 'firebase_options.dart';
 
@@ -13,6 +15,10 @@ void main() {
         primarySwatch: Colors.blue,
       ),
       home: const Homepage(),
+      routes: {
+        '/login/': (context) => const LoginView(),
+        '/register/': (context) => const RegisterView(),
+      },
     ),
   );
 }
@@ -22,35 +28,30 @@ class Homepage extends StatelessWidget {
 
  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-        backgroundColor: Color.fromRGBO(252, 163, 17, 1.0),
+    return FutureBuilder(
+      future: Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
       ),
-      body: FutureBuilder(
-        future: Firebase.initializeApp(
-          options: DefaultFirebaseOptions.currentPlatform,
-        ),
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState){
-            case ConnectionState.done:
-              final user =  FirebaseAuth.instance.currentUser;
-              if (user?.emailVerified ?? false) {
-                print('You are verified');
-              }
-              else{
-                print('You need to be verified');
-              }
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState){
+          case ConnectionState.done:
+            /*  final user =  FirebaseAuth.instance.currentUser;
+            if (user?.emailVerified ?? false) {
               return const Text('Done');
-            default:
-              return const Text('Loading...');
-          }
-          
-        },
-      ),
+            }
+            else{
+              return const VerifyEmailView();
+            } */
+            return const LoginView();
+          default:
+            return const CircularProgressIndicator();
+        }
+        
+      },
     );
   }
 }
+
 
 
 
