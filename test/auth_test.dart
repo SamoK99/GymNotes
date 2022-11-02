684f7f3d24d7ae7b46765356a2166f7b380ad643
SamoK99 <56873602+SamoK99@users.noreply.github.com>
2022-11-02 16:12:25 +0100

    notesService

--- test/auth_test.dart
+++ test/auth_test.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:gymnotes/services/auth/auth_exceptions.dart';
 import 'package:gymnotes/services/auth/auth_provider.dart';
 import 'package:gymnotes/services/auth/auth_user.dart';
